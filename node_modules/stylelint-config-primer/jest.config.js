e27ad6b2254abdccbb47635b34ccc39a8036d38d
amansingla97 <amansingla97@gmail.com>
2019-08-23 00:48:54 +0530

    removed node modules

--- node_modules/stylelint-config-primer/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/*.js', 'plugins/*.js'],
-  setupFilesAfterEnv: ['<rootDir>/__tests__/utils/setup.js'],
-  testPathIgnorePatterns: ['/node_modules/', '__tests__/utils']
-}
