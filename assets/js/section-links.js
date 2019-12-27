557dbc41468a6487f877c18c2d704882d555c634
Lam Jun Rong <bobo999bobo@outlook.com>
2019-12-27 14:24:49 +0800

    Replace ES6 features with ES5 equivalent

--- assets/js/section-links.js
+++ assets/js/section-links.js
@@ -1,34 +1,31 @@
-/*jshint esversion: 6 */
-
-$(document).ready(() => {
-  $("h2,h3,h4,h5,.navigation-list-link").each((index, header) => {
+$(document).ready(function() {
+  $("h2,h3,h4,h5,.navigation-list-link").each(function(index, header) {
     // Skip headers with no id
     if (header.id.length === 0 &&
         !(header.href && header.href.length !== 0)
     ) return;
-    const id = header.id.length === 0 ? header.href : header.id;
+    var id = header.id.length === 0 ? header.href : header.id;
 
-    $(header).append(`
-        <span class="section-link-icon ${header.id.length === 0 ? "navigation-link-icon" : ""}" title="Copy link to this section"
-            data-section-link-id="${id}">&#xe900;</span>
-    `.trim());
+    $(header).append(
+        "<span class='section-link-icon " + (header.id.length === 0 ? "navigation-link-icon" : "") +
+        "' title='Copy link to this section' data-section-link-id='" + id + "'>&#xe900;</span>");
 
     // Hide icons
     $(header).find(".section-link-icon").hide();
-    $(header).hover(() => {
+    $(header).hover(function() {
       // Show on hover
       $(header).find(".section-link-icon").show();
       $(header).find(".section-link-icon").html("&#xe900;");
-    }, () => {
+    }, function() {
       // Hide icons
       $(header).find(".section-link-icon").hide();
     });
   });
 
-  $(".section-link-icon").click(event => {
+  $(".section-link-icon").click(function(event) {
     event.preventDefault();
-    const target = event.target;
-    const id = $(target).attr("data-section-link-id");
+    var target = event.target;
+    var id = $(target).attr("data-section-link-id");
     if ($(target).hasClass("navigation-link-icon")) {
       copyToClipboard(id);
     } else {
@@ -38,16 +35,16 @@ $(document).ready(() => {
   });
 });
 
-const buildUrl = id => {
-  const urlParts = location.href.split("#");
+function buildUrl(id) {
+  var urlParts = location.href.split("#");
   if (urlParts.length > 1) urlParts.pop();
   return urlParts.concat("#" + id).join("");
-};
+}
 
 // Source:
 // https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
-const copyToClipboard = str => {
-  const el = document.createElement('textarea');
+function copyToClipboard(str){
+  var el = document.createElement('textarea');
   el.value = str;
   el.setAttribute('readonly', '');
   el.style.position = 'absolute';
@@ -56,4 +53,4 @@ const copyToClipboard = str => {
   el.select();
   document.execCommand('copy');
   document.body.removeChild(el);
-};
+}
