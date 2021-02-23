37977d7b80d642d58f05438c5409cea48c67646f
Vishwas Singh <singh.vishu234@gmail.com>
2021-02-23 21:31:04 +0530

    [Fix]:dark mode button on homepage

--- assets/js/global_scripts.js
+++ assets/js/global_scripts.js
@@ -4,6 +4,42 @@
 
 $(document).ready(function () {
 
+    //dark mode functionality for home page
+    var aTags = document.querySelectorAll("a.site-button");
+     var searchText = "mode";
+     var storageItem = "colorMode"
+     var a;
+     var isDarkMode = localStorage.getItem(storageItem);
+     for (var i = 0; i < aTags.length; i++) {
+	 if (aTags[i].textContent.search(searchText)>=0) {
+	     a = aTags[i];
+	     break;
+	 }
+     }
+     if (isDarkMode == 0 || isDarkMode == null) {
+	 isDarkMode = 0;
+	 localStorage.setItem(storageItem, isDarkMode);
+     } else if(isDarkMode == 1) {
+	 jtd.setTheme('circuitversedark');
+	 a.textContent = "Light mode";
+     }
+     
+     a.onclick = function() {
+	 if (isDarkMode == 0 || isDarkMode == null) {
+	     jtd.setTheme('circuitversedark');
+	     a.textContent = "Light mode";
+	     isDarkMode = 1;
+	     localStorage.setItem(storageItem, isDarkMode);
+	 } else {
+	     jtd.setTheme('circuitverse');
+	     a.textContent = "Dark mode";
+	     isDarkMode = 0;
+	     localStorage.setItem(storageItem, isDarkMode);
+	 }
+	 return false;
+     }
+
+
     // Collapse active nav lists if opened
     $(".nav-list-expander").click(function() {
         $(this).parent().siblings().removeClass("active");
