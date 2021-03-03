a1746317555d151624e804027be9999b11f19605
Manjot Sidhu <manjot.techie@gmail.com>
2021-03-03 21:02:17 +0530

    Re-write dark mode switching script.
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

--- assets/js/global_scripts.js
+++ assets/js/global_scripts.js
@@ -4,45 +4,44 @@
 
 $(document).ready(function () {
 
-    //dark mode functionality for home page
-    var aTags = document.querySelectorAll("a.site-button");
-     var searchText = "mode";
-     var storageItem = "colorMode";
-     var a;
-     var isDarkMode = localStorage.getItem(storageItem);
-     for (var i = 0; i < aTags.length; i++) {
-	 if (aTags[i].textContent.search(searchText)>=0) {
-	     a = aTags[i];
-	     break;
-	 }
-     }
-     if (isDarkMode == 0 || isDarkMode == null) {
-	 isDarkMode = 0;
-	 localStorage.setItem(storageItem, isDarkMode);
-     } else if(isDarkMode == 1) {
-	 jtd.setTheme('circuitversedark');
-	 a.textContent = "Light mode";
-     }
-     
-     a.onclick = function() {
-	 if (isDarkMode == 0 || isDarkMode == null) {
-	     jtd.setTheme('circuitversedark');
-	     a.textContent = "Light mode";
-	     isDarkMode = 1;
-	     localStorage.setItem(storageItem, isDarkMode);
-	 } else {
-	     jtd.setTheme('circuitverse');
-	     a.textContent = "Dark mode";
-	     isDarkMode = 0;
-	     localStorage.setItem(storageItem, isDarkMode);
-	 }
-	 return false;
-     };
+	//dark mode functionality
+	var a = $('a.site-button:contains("mode")');
 
+	// Switch Color Scheme as soon as possible
+	var searchText = "mode";
+	var storageItem = "colorMode";
+	var isDarkMode = localStorage.getItem(storageItem);
 
-    // Collapse active nav lists if opened
-    $(".nav-list-expander").click(function() {
-        $(this).parent().siblings().removeClass("active");
-    });
+	if (isDarkMode == 0 || isDarkMode == null) {
+		isDarkMode = 0;
+		localStorage.setItem(storageItem, isDarkMode);
+	} else if (isDarkMode == 1) {
+		jtd.setTheme('circuitversedark');
+		a.text("Light mode");
+	}
+
+	a.click(function () {
+		
+		if (isDarkMode == 0 || isDarkMode == null) {
+			jtd.setTheme('circuitversedark');
+			a.text("Light mode");
+			isDarkMode = 1;
+			localStorage.setItem(storageItem, isDarkMode);
+		} else {
+			jtd.setTheme('circuitverse');
+			a.text("Dark mode");
+			isDarkMode = 0;
+			localStorage.setItem(storageItem, isDarkMode);
+		}
+
+		// Reset Disqus thread to reload with matching color scheme
+		setTimeout(function(){ DISQUS.reset({reload: true}); }, 500);
+		return false;
+	});
+
+	// Collapse active nav lists if opened
+	$(".nav-list-expander").click(function () {
+		$(this).parent().siblings().removeClass("active");
+	});
 
 });
